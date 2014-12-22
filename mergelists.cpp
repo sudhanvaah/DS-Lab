9882b8a1a39f5a45b098d3af13979d55ea3327c2
Sudhanva <sudhanvaah@gmail.com>
2014-12-22 20:06:38 +0530

    The merge function was incorrect. It's corrected now.

--- mergelists.cpp
+++ mergelists.cpp
@@ -1,114 +1,111 @@
-//Merge two ordered linked lists
 #include<iostream>
 #include<stdlib.h>
 using namespace std;
 struct node
 {
-    int data;
-    node* link;
+int data;
+node* link;
 };
 typedef node * NODE;
 NODE getnode()
 {
-    NODE x=(NODE)malloc(sizeof(node));
-    return x;
+NODE x=(NODE)malloc(sizeof(node));
+return x;
 }
 NODE insert_rear(int t,NODE root)
 {
-    NODE temp=getnode(),r=root;
-    temp->data=t;
-    temp->link=NULL;
-    if(root==NULL)
-        return temp;
-    while(r->link!=NULL)
-        r=r->link;
-    r->link=temp;
-    return root;
+NODE temp=getnode(),r=root;
+temp->data=t;
+temp->link=NULL;
+if(root==NULL)
+return temp;
+while(r->link!=NULL)
+r=r->link;
+r->link=temp;
+return root;
 }
 NODE merge_(NODE root1,NODE root2)
 {
-    NODE root,temp,temp2;
-    if(root1==NULL)
-        return root2;
-    if(root2==NULL)
-        return root1;
-    if(root1->data<=root2->data)
-        root=root1;
-    else
+    NODE temp=root1,root=root1,r=root1;
+    int t;
+    while(temp->link!=NULL)
+        temp=temp->link;
+    temp->link=root2;
+    while(root->link!=NULL)
     {
-        root=root2;
-        root2=root1;
-        root1=root;
-    }
-    temp=root;
-    root1=root1->link;
-    while(root2!=NULL && root1!=NULL)
-    {
-        if(root2->data<root1->data)
-        {
-            temp2=root2;
-            root2=root2->link;
-            temp2->link=temp->link;
-            temp->link=temp2;
-            temp=temp->link;
-        }
-        temp=root1;
-        root1=root1->link;
-    }
-    if(root2!=NULL)
-    {
-        temp->link=root2;
-    }
-    return root;
+            r=root;
+            if(r->link->link==NULL)
+            {
+                if(r->data>r->link->data)
+                 {
+                     t=r->data;
+                     r->data=r->link->data;
+                     r->link->data=t;
+                 }
 
+            }
+            while(r->link!=NULL)
+            {
+            if(root->data>r->data)
+            {
+                 t=r->data;
+                 r->data=root->data;
+                 root->data=t;
+            }
+            r=r->link;
+            }
+        root=root->link;
+    }
+    return root1;
 }
 void disp(NODE root)
 {
-    if(root==NULL)
-     {
-      cout<<"\nEmpty list";
-      return;
-     }
-    cout<<"\nList is ";
-    while(root!=NULL)
-        {
-         cout<<root->data<<" ";
-         root=root->link;
-        }
+if(root==NULL)
+{
+cout<<"\nEmpty list";
+return;
+}
+cout<<"\nList is ";
+while(root!=NULL)
+{
+cout<<root->data<<" ";
+root=root->link;
+}
 }
 main()
 {
- NODE root1=NULL,root2=NULL,root;
- int i=1;
- int n;
- cout<<"\nEnter first list";
- while(i)
- {
+NODE root1=NULL,root2=NULL,root;
+int i=1;
+int n;
+cout<<"\nEnter first list";
+while(i)
+{
+cout<<"\n1 to enter element\n0 to finish entering ";
+cin>>i;
+if(i==1)
+{
+cout<<"\nEnter element ";
+cin>>n;
+root1=insert_rear(n,root1);
+}
+}
+i=1;
+cout<<"\nEnter second list";
+while(i)
+{
+cout<<"\n1 to enter element\n0 to finish entering ";
+cin>>i;
+if(i==1)
+{
+cout<<"\nEnter element ";
+cin>>n;
+root2=insert_rear(n,root2);
+}
+}
+disp(root1);
+disp(root2);
+root=merge_(root2,root1);
 
-	 cout<<"\n1 to enter element\n0 to finish entering ";
-	 cin>>i;
-	 if(i==1)
-		  {
-			cout<<"\nEnter element ";
-			cin>>n;
-			root1=insert_rear(n,root1);
-		  }
- }
- i=1;
- cout<<"\nEnter second list";
- while(i)
- {
-
-	 cout<<"\n1 to enter element\n0 to finish entering ";
-	 cin>>i;
-	 if(i==1)
-		  {
-			cout<<"\nEnter element ";
-			cin>>n;
-			root2=insert_rear(n,root2);
-		  }
- }
- root=merge_(root2,root1);
- disp(root);
+disp(root);
 return 0;
 }
